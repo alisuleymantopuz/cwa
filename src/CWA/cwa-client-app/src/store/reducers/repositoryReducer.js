ec593579ee032accc23ccd77cf591ec302c8a221
Ali Suleyman Topuz <alisuleymantopuz@gmail.com>
2020-12-25 20:17:43 +0200

    setup axios, redux and react-redux install, action types and action containers and reducers initialized, registering reducer file with thunk

---
+++ src/CWA/cwa-client-app/src/store/reducers/repositoryReducer.js
@@ -0,0 +1,48 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    data: null,
+    showSuccessModal: false
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_DATA_SUCCESS:
+            return executeGetDataSuccess(state, action);
+        case actionTypes.POST_DATA_SUCCESS:
+            return executePostDataSuccess(state, action);
+        case actionTypes.PUT_DATA_SUCCESS:
+            return executePutDataSuccess(state, action);
+        case actionTypes.DELETE_DATA_SUCCESS:
+            return executeDeleteDataSuccess(state, action);
+        default:
+            return state;
+    }
+}
+
+const executeGetDataSuccess = (state, action) => {
+    return {
+        ...state,
+        data: action.data
+    }
+}
+const executePostDataSuccess = (state, action) => {
+    return {
+        ...state,
+        showSuccessModal: true
+    }
+}
+const executePutDataSuccess = (state, action) => {
+    return {
+        ...state,
+        showSuccessModal: true
+    }
+}
+const executeDeleteDataSuccess = (state, action) => {
+    return {
+        ...state,
+        showSuccessModal: true
+    }
+}
+
+export default reducer;
